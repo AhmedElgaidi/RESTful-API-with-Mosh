592d3817382711372c062a4974b862e3bbebba29
Ahmed Elgaidi <49118737+AhmedElgaidi@users.noreply.github.com>
2021-09-10 14:57:09 +0200

    Delete auth directory

--- auth/middlewares/admin.js
+++
@@ -1,6 +0,0 @@
-const isAdmin = (req, res, next) => {
-    // 403 means forbidden while 4041 is unauthorized
-    if(!req.user.isAdmin) return res.status(403).send('Acces denied.');
-    next();
-};
-module.exports = isAdmin;
